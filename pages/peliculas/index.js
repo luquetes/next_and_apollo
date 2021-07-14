a491d55855b980bda0fd967e830d3d7edacd6ca3
= <lucas.lema.91@gmail.com>
2021-07-14 12:47:14 -0300

    getStaticProps

--- pages/peliculas/index.js
+++ pages/peliculas/index.js
@@ -1,22 +1,31 @@
 import PeliculasList from '../../components/peliculas/PeliculasList'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 import { DUMMY_MOVIES } from '../../constants/DUMMY_MOVIES'
 
-const Peliculas =  () => {
+const Peliculas = props => {
 
-    const [loadedPeliculas, setLoadedPeliculas] = useState([])
+    // const [loadedPeliculas, setLoadedPeliculas] = useState([])
 
-    useEffect(() => {
-        // http request
-        // Hacer este approach clasico de react no nos va a dar la info en el codigo fuente. Para hacer el Pre-Rendering, cumpliendo con el SSR, necesitamos otra cosa :D
-        setLoadedPeliculas(DUMMY_MOVIES)
-    }, [])
+    // useEffect(() => {
+    //     // http request
+    //     // Hacer este approach clasico de react no nos va a dar la info en el codigo fuente. Para hacer el Pre-Rendering, cumpliendo con el SSR, necesitamos otra cosa :D
+    //     setLoadedPeliculas(DUMMY_MOVIES)
+    // }, [])
 
     return <>
         <h1>Esta es la seccion de peliculas</h1>
-        <PeliculasList peliculas={loadedPeliculas} />
+        <PeliculasList peliculas={props.peliculas} />
     </>
+}
+
+export async function getStaticProps() {
+    return {
+        props: {
+            peliculas: DUMMY_MOVIES,
+            revalidate: 10
+        }
+    }
 }
 
 export default Peliculas
